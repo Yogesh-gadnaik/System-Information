cb15b53a773ff1c1625cf84751b9568abf81efbe
Yogesh-gadnaik <yogeshga.in@gmail.com>
2023-03-06 11:05:44 +0530

    Added login page in front and made some chnages in all files

--- app.py
+++ app.py
@@ -1,29 +1,55 @@
 
 from flask import Flask, render_template, request
+from logic.getsysinfo import sys_name, database_check, insert_data
 
-from handlers.handlers import Load_Home_Page, Load_Root_Page
 
 apk = Flask(__name__)
 
 
 @apk.route('/')
 def root():
-    page_Name = Load_Root_Page()
-    return render_template(page_Name)
+    return render_template('login.html')
+
+
+@apk.route('/validation', methods=['POST', 'GET'])
+def validation():
+    uname = request.form['name']
+    password = request.form['pass']
+    value = database_check(uname, password)
+    if value == 1:
+        return render_template('index.html')
+    else:
+        # return redirect('/')
+        return render_template('invalid.html')
 
 
 @apk.route('/home', methods=['POST', 'GET'])
 def load_home():
-    if request.method == 'POST':
-        system_info = request.form['select_val']
-        resp = Load_Home_Page(system_info)
-        if system_info == "allinfo":
-            return render_template('all_info_response.html', result=resp)
-        else:
-            return render_template('response.html', result=resp)
+    system_info = request.form['select_val']
+    response = sys_name(system_info)
+    if system_info == "allinfo":
+        return render_template('all_info_response.html', result=response)
     else:
-        return "<html><body><h1>Error</h1></body></html>"
+        return render_template('response.html', result=response)
+
+
+@apk.route('/new')
+def new_register():
+    return render_template('register.html')
+
+
+@apk.route('/register', methods=['POST', 'GET'])
+def register():
+    name = request.form['name']
+    password = request.form['pass']
+    age = request.form['age']
+    gender = request.form['select_gender']
+    response = insert_data(name, password, age, gender)
+    if response == 1:
+        return render_template('login.html')
+    else:
+        return render_template('register.html')
 
 
 if __name__ == '__main__':
-    apk.run(debug=True)
+    apk.run(debug=True, port=5001)
--- logic/getsysinfo.py
+++ logic/getsysinfo.py
@@ -1,18 +1,42 @@
 from model.dictinory import init_dictionary
+import pypyodbc
+
+connection = pypyodbc.connect("""Driver={SQL Server};
+                           Server=172.16.10.45;
+                           Database=yogesh;
+                           uid=talend2019;
+                           pwd=Mouri@123;""")
 
 
 def sys_name(info):
     sys_info = init_dictionary()
-    try:
-        if info == "allinfo":
-            dict = {}
-            for i, j in sys_info.items():
-                dict[i] = j
-            return dict
+    if info == "allinfo":
+        return sys_info
+    else:
+        return sys_info[info], info
 
-        else:
-            return sys_info[info], info
 
-    except Exception as e:
-        e = "Please select proper option"
-        return e
+def database_check(uname, password):
+
+    cursor = connection.cursor()
+    cursor.execute("select * from login_validation where Name='" +
+                   uname+"' and Password='"+password+"'")
+    data = cursor.fetchone()
+    if data:
+        return 1
+    else:
+        return 0
+
+
+def insert_data(uname, password, age, gender):
+
+    cursor = connection.cursor()
+    cursor.execute("insert into login_validation(Name,Password,Age,Gender) values('" +
+                   uname+"','"+password+"',"+str(age)+",'"+gender+"')")
+    cursor.commit()
+    cursor.execute("select * from login_validation where Name='" + uname+"'")
+    data = cursor.fetchone()
+    if data:
+        return 1
+    else:
+        return 0
